f96364d1b7b1b5907d57c010a5e712846b25d69b
BasiaM1 <b.murzynowska@wp.pl>
2020-05-12 22:40:44 +0200

    Add links to sidebar

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-"use strict";
-{
+'use strict'; {
   const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
-    console.log("Link was clicked!");
+    console.log('Link was clicked!');
     console.log(event);
 
     /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll(".titles a.active");
+    const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
-      activeLink.classList.remove("active");
+      activeLink.classList.remove('active');
     }
 
     /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll(".posts article.active");
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
-      activeArticle.classList.remove("active");
+      activeArticle.classList.remove('active');
     }
 
     /* add class 'active' to the clicked link */
-    console.log("clickedElement:", clickedElement);
-    console.log("clickedElement (with plus): " + clickedElement);
-    clickedElement.classList.add("active");
+    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
 
     /* get 'href' attribute from the clicked link */
-    const articleSelector = clickedElement.getAttribute("href");
+    const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
@@ -33,12 +32,54 @@
     console.log(targetArticle);
 
     /* add class 'active' to the correct article */
-    targetArticle.classList.add("active");
+    targetArticle.classList.add('active');
   };
 
-  const links = document.querySelectorAll(".titles a");
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-  for (let link of links) {
-    link.addEventListener("click", titleClickHandler);
+  function generateTitleLinks() {
+    console.log('function is runnning');
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList);
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for (let article of articles) {
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* find the title element and get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHTML =
+        '<li><a href="#' +
+        articleId +
+        '"><span>' +
+        articleTitle +
+        '</span></a></li>';
+      console.log(linkHTML);
+      /* insert link into titleList */
+      html = html + linkHTML;
+      // titleList.insertAdjacentHTML("afterend", linkHTML); jak to zastosować?
+      // console.log(html);
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
   }
+  generateTitleLinks();
 }
